d1cb05038727bd0d176f2a5d51041049bbccfa44
efisher5 <ecfisher13@gmail.com>
2024-05-18 18:53:32 -0400

    set up node server 2

--- producer/src/index.ts
+++ producer/src/index.ts
@@ -1,9 +1,13 @@
+import { Queue } from "bullmq";
 import express from "express";
 
+// Express setup
 const app = express();
-
 const port = 3000;
 
+// Queue setup
+const queue = new Queue('Cities');
+
 app.listen(port, () => {
-    console.log(`Server listening on port ${port}...`)
+    console.log(`Server listening on port ${port}...`);
 })
